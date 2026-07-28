53e23c8876e0bb15f6d46f1f625200d6f88970ab
SlippyJs <juanitoalcachofa_100@outlook.com>
2026-07-28 12:41:46 -0500

    chore: initialize PUCE Market API

---
+++ src/main/kotlin/ec/edu/puce/pucemarket/PuceMarketApplication.kt
@@ -0,0 +1,11 @@
+package ec.edu.puce.pucemarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PuceMarketApplication
+
+fun main(args: Array<String>) {
+    runApplication<PuceMarketApplication>(*args)
+}
---
+++ src/test/kotlin/ec/edu/puce/pucemarket/PuceMarketApplicationTests.kt
@@ -0,0 +1,13 @@
+package ec.edu.puce.pucemarket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PuceMarketApplicationTests {
+
+    @Test
+    fun contextLoads() = Unit
+}
